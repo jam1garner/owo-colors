6b6a13396785d3f980c1e48ca2ec2687b5b99848
SirWindfield <SirWindfield@users.noreply.github.com>
2021-03-26 18:29:09 +0100

    style: run cargo fmt

--- examples/is_tty.rs
+++ examples/is_tty.rs
@@ -1,5 +1,8 @@
 use owo_colors::OwoColorize;
 
 fn main() {
-    println!("{}", "This will be red if viewed through a tty!".if_stdout_tty(|x| x.red()));
+    println!(
+        "{}",
+        "This will be red if viewed through a tty!".if_stdout_tty(|x| x.red())
+    );
 }
